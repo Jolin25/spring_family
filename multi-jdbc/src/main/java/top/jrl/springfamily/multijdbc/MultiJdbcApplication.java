8c53b0a67587689ea3dffc43580980c3c8a1c5c0
JolyPhone <18990341730@163.com>
2022-02-16 21:29:02 +0800

    init

---
+++ multi-jdbc/src/main/java/top/jrl/springfamily/multijdbc/MultiJdbcApplication.java
@@ -0,0 +1,13 @@
+package top.jrl.springfamily.multijdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiJdbcApplication.class, args);
+    }
+
+}
---
+++ multi-jdbc/src/test/java/top/jrl/springfamily/multijdbc/MultiJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package top.jrl.springfamily.multijdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
