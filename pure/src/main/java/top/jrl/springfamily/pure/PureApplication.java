37c653413cbd840123b52a6f390632d9fb35ead8
JolyPhone <18990341730@163.com>
2022-02-17 08:32:13 +0800

    jdbc

---
+++ pure/src/main/java/top/jrl/springfamily/pure/PureApplication.java
@@ -0,0 +1,13 @@
+package top.jrl.springfamily.pure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PureApplication.class, args);
+    }
+
+}
